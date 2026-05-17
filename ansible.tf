931917f751c6b6f04dc7c2e6ce74408144111153
fubuki <towatakdeda1016@docomo.ne.jp>
2026-05-17 10:24:47 +0900

    javaupdate

--- ansible.tf
+++ ansible.tf
@@ -1,6 +1,13 @@
-resource "local_file" "ansible_inventory" {
-  content = <<EOT
+resource "null_resource" "create_ansible_dir" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.module}/ansible"
+  }
+}
 
+resource "local_file" "ansible_inventory" {
+  depends_on = [null_resource.create_ansible_dir]
+
+  content = <<EOT
 [web]
 ${aws_instance.springboot.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=${var.ssh_private_key_path}
 EOT
