137370421fd91ae81c410a051a88cede19b6aa81
fubuki <towatakdeda1016@docomo.ne.jp>
2026-05-18 23:57:12 +0900

    共有

---
+++ terraform/ansible.tf
@@ -0,0 +1,29 @@
+resource "null_resource" "create_ansible_dir" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.module}/../ansible"
+  }
+}
+
+resource "local_file" "ansible_inventory" {
+  depends_on = [null_resource.create_ansible_dir]
+
+  content = <<EOT
+[web]
+${aws_instance.springboot.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=${var.ssh_private_key_path}
+EOT
+
+  filename = "${path.module}/../ansible/inventory.ini"
+}
+
+resource "null_resource" "run_ansible" {
+
+  depends_on = [local_file.ansible_inventory]
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${path.module}/../ansible/inventory.ini ${path.module}/../ansible/deploy_app.yml"
+  }
+}
