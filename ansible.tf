6daeff05262a5259547883ac41aa17d555ac4c68
fubuki <towatakdeda1016@docomo.ne.jp>
2026-05-19 13:37:44 +0900

    ansible.tf修正

--- ansible.tf
+++ ansible.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "create_ansible_dir" {
   provisioner "local-exec" {
-    command = "mkdir -p ${path.module}/../ansible"
+    command = "mkdir -p ${path.module}/ansible"
   }
 }
 
@@ -12,7 +12,7 @@ resource "local_file" "ansible_inventory" {
 ${aws_instance.springboot.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=${var.ssh_private_key_path}
 EOT
 
-  filename = "${path.module}/../ansible/inventory.ini"
+  filename = "${path.module}/ansible/inventory.ini"
 }
 
 resource "null_resource" "run_ansible" {
@@ -24,6 +24,6 @@ resource "null_resource" "run_ansible" {
   }
 
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${path.module}/../ansible/inventory.ini ${path.module}/../ansible/deploy_app.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${path.module}/ansible/inventory.ini ${path.module}/ansible/deploy_app.yml"
   }
 } 
